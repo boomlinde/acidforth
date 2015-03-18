3ca2c46985586c21a3a3b3cc7e94b83a409621e9
Philip Linde <plinde@proceranetworks.com>
2015-03-18 23:16:08 +0100

    remove components.go

--- acidforth.go
+++ acidforth.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"code.google.com/p/portaudio-go/portaudio"
+	"fmt"
 	"github.com/boomlinde/acidforth/collection"
 	"github.com/boomlinde/acidforth/machine"
+	"github.com/boomlinde/acidforth/machine/stack"
+	"github.com/boomlinde/acidforth/seq"
+	"github.com/boomlinde/acidforth/synth"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"time"
 )
 
-const SFREQ = 44100
+const sfreq = 44100
 
 func main() {
 	log.Println("Booting")
 	col := collection.NewCollection()
-	addComponents(SFREQ, col)
+	addComponents(sfreq, col)
 
 	log.Println("Waiting for source on stdin")
 	data, err := ioutil.ReadAll(os.Stdin)
@@ -31,7 +36,7 @@ func main() {
 
 	portaudio.Initialize()
 	defer portaudio.Terminate()
-	stream, err := portaudio.OpenDefaultStream(0, 2, SFREQ, 0, col.Callback)
+	stream, err := portaudio.OpenDefaultStream(0, 2, sfreq, 0, col.Callback)
 	chk(err)
 	defer stream.Close()
 	stream.Start()
@@ -39,6 +44,28 @@ func main() {
 	for {
 		time.Sleep(time.Second)
 	}
+}
+
+func addComponents(srate float64, c *collection.Collection) {
+	for i := 1; i < 9; i++ {
+		_ = synth.NewOperator(fmt.Sprintf("op%d", i), c, srate)
+		_ = synth.NewEnvelope(fmt.Sprintf("env%d", i), c, srate)
+	}
+	for _, r := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		_ = synth.NewRegister(string(r), c)
+	}
+	for i := 1; i < 5; i++ {
+		_ = synth.NewAccumulator(fmt.Sprintf("mix%d", i), c)
+	}
+
+	_ = seq.NewSeq("seq", c, srate)
+
+	synth.NewWaveTables(c)
+
+	c.Machine.Register("srate", func(s *stack.Stack) { s.Push(srate) })
+	c.Machine.Register("m2f", func(s *stack.Stack) {
+		s.Push(440 * math.Pow(2, (s.Pop()-69)/12))
+	})
 }
 
 func chk(err error) {
